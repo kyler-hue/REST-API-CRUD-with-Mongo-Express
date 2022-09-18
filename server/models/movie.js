682bf21097fc707f008c05f1810286ab16fd0113
Vishwajeet Jaysing Pawar <82999574+kyler-hue@users.noreply.github.com>
2022-09-18 13:53:34 +0530

    Add files via upload

---
+++ server/models/movie.js
@@ -0,0 +1,27 @@
+
+const mongoose = require('mongoose') ;
+
+const movieSchema = new mongoose.Schema({
+
+    name:{
+        type:String,
+        required:'This field is required'
+    },
+    description:{
+        type:String,
+        required:true 
+    },
+    category:{
+        type:Array,
+        required:true
+    },
+    thumbnail:{
+        type:String,
+        required:true
+    }
+});
+
+
+
+module.exports = mongoose.model('Movie',movieSchema) ;
+
